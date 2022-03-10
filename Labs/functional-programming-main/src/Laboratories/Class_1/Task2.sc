c9cd7d6ce97352893858854fb17630a16bc88b3b
shnartho <shnartho@gmail.com>
2022-03-11 00:36:57 +0100

    updated

--- Labs/functional-programming-main/src/Laboratories/Class_1/Task2.sc
+++
@@ -1,6 +0,0 @@
-def multiply(a: Int, b: Int) = a * b
-
-multiply(2, 2) // expect 4
-multiply(2, 4) // expect 8
-multiply(4, 2) // expect 8
-multiply(20, 2) // expect 40
--- Tutorials/Exercises/List_1_2/HelloWorld.scala
+++
@@ -1,8 +0,0 @@
-package Exercises.List_1_2
-
-object HelloWorld {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-    println(args(0))
-  }
-}
